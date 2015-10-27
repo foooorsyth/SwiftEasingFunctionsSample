62097b1eafd472eb9f2434b001a60b49811ca7ba
Justin Forsyth <justinforsyth@macbook-air.local>
2015-10-27 00:13:06 -0400

    Initial Commit

---
+++ EasingExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasingExample
+//
+//  Created by Justin Forsyth on 10/27/15.
+//  Copyright © 2015 jforce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
